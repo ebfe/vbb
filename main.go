28fb644f08f7b9b3764c2b95603070f7085fd8ad
Michael Gehring <mg@ebfe.org>
2017-07-19 20:56:17 +0000

    show build status

--- main.go
+++ main.go
@@ -13,6 +13,32 @@ import (
 
 var debug = flag.Bool("d", false, "debug")
 
+func lastBuildStatus(builder string) (string, error) {
+	for i := -1; i > -3; i-- {
+		u := fmt.Sprintf("https://build.voidlinux.eu/json/builders/%s/builds/%d", builder, i)
+		rsp, err := http.Get(u)
+		if err != nil {
+			return "", err
+		}
+		defer rsp.Body.Close()
+
+		if rsp.StatusCode != http.StatusOK {
+			return "", fmt.Errorf("http status: %d - %s\n", rsp.StatusCode, rsp.Status)
+		}
+
+		var doc struct {
+			Text []string `json:"text"`
+		}
+		d := json.NewDecoder(rsp.Body)
+		err = d.Decode(&doc)
+		if err != nil {
+			return "", err
+		}
+		return strings.Join(doc.Text, " "), nil
+	}
+	return "", nil
+}
+
 func do() {
 	rsp, err := http.Get("https://build.voidlinux.eu/json/builders")
 	if err != nil {
@@ -36,6 +62,8 @@ func do() {
 	var doc map[string]struct {
 		State   string `json:"state"`
 		Pending int    `json:"pendingBuilds"`
+		Current []int  `json:"currentBuilds"`
+		Cached  []int  `json:"cachedBuilds"`
 	}
 
 	d := json.NewDecoder(r)
@@ -52,10 +80,16 @@ func do() {
 
 	for _, builder := range builders {
 		info := doc[builder]
-		builder = strings.Replace(builder, "_builder", "", 1)
-		fmt.Printf("%12s: %9s", builder, info.State)
+		arch := strings.Replace(builder, "_builder", "", 1)
+		fmt.Printf("%12s: %9s", arch, info.State)
 		if info.Pending != 0 {
 			fmt.Printf(" %3d pending", info.Pending)
+		}
+		last, err := lastBuildStatus(builder)
+		if err != nil {
+		}
+		if last != "" {
+			fmt.Printf(" | %s", last)
 		}
 		fmt.Println()
 	}
